9d0c83d02224fac51c263e7bc0b48cf1c4f90b8d
WebCompanyOKA <41568667+WebCompanyOKA@users.noreply.github.com>
2019-07-30 15:32:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import './index.css';
 import App from './App';
 import Modal from './component/modal2';
 import Menu from './component/glav-menu';
-import Start from './component/Start';
-//import Start2 from './component/Start2';
 import * as serviceWorker from './serviceWorker';
 
 //  ReactDOM.render(<Modal />, document.getElementById('modal'));
 ReactDOM.render(<Menu />, document.getElementById('Menu'));
 ReactDOM.render(<App />, document.getElementById('root'));
-//ReactDOM.render(<Start2 />, document.getElementById('Start2'));
 
 // ReactDOM.render(<Modal />, document.getElementById('modal'));
 
